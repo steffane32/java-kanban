9f45e93353101cf3725c8fa66a5f321c7a77753c
Olga Zavalishina <steffane@yandex.ru>
2025-04-05 14:36:58 +0300

    homeWork sprint4 ver1.0

---
+++ src/Epic.java
@@ -0,0 +1,5 @@
+public class Epics extends Tasks {
+    public Epics(int id, String name, String description, Status status) {
+        super(id, name, description, status);
+    }
+}
---
+++ src/SubTask.java
@@ -0,0 +1,5 @@
+public class SubTasks extends Tasks {
+    public SubTasks(int id, String name, String description, Status status) {
+        super(id, name, description, status);
+    }
+}
