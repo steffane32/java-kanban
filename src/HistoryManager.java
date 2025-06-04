f9a83021be99892c9fd65a14211246fb954addab
Olga Zavalishina <steffane@yandex.ru>
2025-06-04 15:51:14 +0300

    sprint6 homeWorkVer1.5 style

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -1,7 +1,6 @@
 import java.util.List;
-
 public interface HistoryManager {
-void add(Task task); // Добавляет задачу в историю
-List<Task> getHistory(); // Возвращает историю просмотров
-void remove(int id); // Удаляет задачу из истории (пригодится позже)
+    void add(Task task); // Добавляет задачу в историю
+    List<Task> getHistory(); // Возвращает историю просмотров
+    void remove(int id); // Удаляет задачу из истории (пригодится позже)
 }
