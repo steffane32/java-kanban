1d1ed027c5fea1559c48e3cf04884009d5f45e8d
Olga Zavalishina <steffane@yandex.ru>
2025-08-06 16:31:57 +0300

    sprint8 homeWorkVer1.2 main renew

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.time.Duration;
-import java.time.LocalDateTime;
+//import java.time.Duration;
+//import java.time.LocalDateTime;
 //самопроверка
 public class Main {
     public static void main(String[] args) {
