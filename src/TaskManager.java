d958685f55a93eb68c6946d6f3fd68b4b4a896c6
Olga Zavalishina <steffane@yandex.ru>
2025-08-06 16:14:48 +0300

    sprint8 homeWorkVer1.1 separated

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,37 +1,55 @@
 import java.util.List;
 
 public interface TaskManager {
+
     // Методы для Task
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTaskById(int id);
+
     Task createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTaskById(int id);
+
 
     // Методы для Epic
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpicById(int id);
+
     Epic createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpicById(int id);
+
 
     // Методы для SubTask
     List<SubTask> getAllSubtasks();
+
     void deleteAllSubtasks();
+
     SubTask getSubtaskById(int id);
+
     SubTask createSubtask(SubTask subtask);
+
     void updateSubtask(SubTask subtask);
+
     void deleteSubtaskById(int id);
+
 
     // Дополнительные методы
     List<SubTask> getSubtasksForEpic(int epicId);
+
     List<Task> getHistory();
 
-    // Новый метод для получения приоритизированных задач
     List<Task> getPrioritizedTasks();
 
-    // Метод для полной очистки
     void clearAll();
 }
